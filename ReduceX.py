d6b46848b9937720ce387a15e8ac655a1c812fbf
Lahuraj Wadkar <lahuraj.wadkar@gmail.com>
2026-06-28 12:27:54 +0530

    Implement filter, map, and reduce in ReduceX.py
    Fix syntax errors and improve functionality in ReduceX.py

---
+++ ReduceX.py
@@ -0,0 +1,20 @@
+from functools import reduce
+
+chkEven = lambda no:(no%2==0)
+
+Inclrement = lambda no:(no+1)
+
+def Addition(no1, no2):
+  return no1 + no2
+  
+def main():
+  Data = [1,3,2,5 7,10]
+  fData = list(filter(chkEven, Data))
+  print("Data after fitler:", fData)
+  MData = list(map(Increment, fData))
+  print("Data after map:", MData)
+  rData = reduce(Addition,  MData)
+  print("Data after reduce :", rData)
+  
+if(__name__ == "__main__"):
+  main()
